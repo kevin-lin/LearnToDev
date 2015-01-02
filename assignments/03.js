55d972004ad7f9b8cd06e55e650879eaf7e6ca86
Kevin Lin <whatakevin@gmail.com>
2015-01-02 14:11:25 -0600

    Create Assignment 3

---
+++ assignments/03.js
@@ -0,0 +1,13 @@
+/*
+ * taken from http://www.cplusplus.com/forum/articles/12974/
+ * Requires:
+ * variables, data types, and numerical operators
+ * basic input/output
+ * logic (if statements, switch statements)
+ * 
+ * Write a program that allows the user to enter the grade scored in a programming class (0-100).
+ * If the user scored a 100 then notify the user that they got a perfect score.
+ * Bonus:
+ *   Modify the program so that if the user scored a 90-100 it informs the user that they scored an A
+ *   Modify the program so that it will notify the user of their letter grade. 0-59 F 60-69 D 70-79 C 80-89 B 90-100 A
+ */
